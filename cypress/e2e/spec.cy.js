1f7fe7f8cab08406a8c03fe983a780a3b8d32d8b
Marta Anna Todorovic <martaannatodorovic@gmail.com>
2023-03-20 02:43:46 +0100

    link changed in test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,21 +4,21 @@ describe('Express testing', () => {
     });
   
     it('Visits github pages deployment', () => {
-      cy.visit('https://solidtop.github.io/esc-hacker-escape-rooms/');
+      cy.visit('https://matodorovic.github.io/esc-cypress/');
     });
   
     it('Goes to challenges by clicking on Play on-site', () => {
-      cy.visit('https://solidtop.github.io/esc-hacker-escape-rooms/');
+      cy.visit('https://matodorovic.github.io/esc-cypress/');
       cy.get('.main-menu:nth-child(2) a').first().click();
       cy.url().should('include', '/challenges');
     });
 
     it('Visits The story sites on github pages', () => {
-      cy.visit('https://solidtop.github.io/esc-hacker-escape-rooms/about.html');
+      cy.visit('https://matodorovic.github.io/esc-cypress/about.html');
     });
 
     it ('Goes to contact page', () => {
-      cy.visit('https://solidtop.github.io/esc-hacker-escape-rooms/contact.html');
+      cy.visit('https://matodorovic.github.io/esc-cypress/contact.html');
       cy.contains('type').click();
       cy.url().should('include', '/contact.html');
     });
